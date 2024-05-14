a533250121b722675c1f6f9931763fcfb2a80163
LeoDominici71 <leonardo_requena@outlook.com>
2024-05-14 13:34:50 -0300

    first commit

---
+++ src/main/java/com/fiap/apiGatewayZuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,15 @@
+package com.fiap.apiGatewayZuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fiap/apiGatewayZuul/config/GatewayConfig.java
@@ -0,0 +1,22 @@
+package com.fiap.apiGatewayZuul.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("userManagement", r -> r.path("/userManagement/api/user/**")
+                .uri("lb://userManagement"))
+            .route("productRegistry", r -> r.path("/productRegistry/api/products/**")
+                .uri("lb://productRegistry"))
+            .route("orderManagement", r -> r.path("/orderManagement/api/order/**")
+            	.uri("lb://orderManagement"))
+            .build();
+    }
+}
